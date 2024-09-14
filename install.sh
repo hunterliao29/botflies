d5e0621b2e66a591b5d6fa811dae05203de4fdfa
jliaoh <48660001+hunterliao29@users.noreply.github.com>
2024-09-13 22:30:30 -0500

    typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME'
-git clone --bare https://github.com/hunterliao29/botfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/hunterliao29/dotfiles.git $HOME/.dotfiles
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
 
